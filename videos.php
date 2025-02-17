393c5e38f100a457459710141a3044a83e5c0d9d
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-02-17 23:04:24 +0600

    videos

---
+++ videos.php
@@ -0,0 +1,64 @@
+{
+  "object": "page",
+  "entry": [
+    {
+      "time": 1739811694607,
+      "id": "144335542089831",
+      "messaging": [
+        {
+          "sender": {
+            "id": "6399073443526400"
+          },
+          "recipient": {
+            "id": "144335542089831"
+          },
+          "timestamp": 1739811693950,
+          "message": {
+            "mid": "m_HbNRrGn4Vqx-cCH4mfmNQicPHzTsTdbAhaPpG97qlO8K8-EL00K-x5koO7CnOfh9B2ZtIzqHYQeoD-ghHN4Ghg",
+            "attachments": [
+              {
+                "type": "video",
+                "payload": {
+                  "url": "https://video.xx.fbcdn.net/v/t42.3356-2/464285700_8463644337081449_4925236397587452105_n.mp4?_nc_cat=109&ccb=1-7&_nc_sid=4f86bc&_nc_ohc=fkMLMRPg188Q7kNvgHAJCob&_nc_oc=AdjrKp84DuvR35OSd1Ex4_hWV2Zh5VyIGG3-ZkkVP8wEdIxV40UMVcF6tbGw-976JYbnTTYWom3pYhZVOH_lviIt&_nc_zt=28&_nc_ht=video.xx&_nc_gid=A-EbQAU1KtWCFyKf_R5aP8W&oh=03_Q7cD1gHuv8nsDZj4M77mWBDbS4SOa-Mi5_yQfAdPdRQl7FL77A&oe=67B54C16"
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  ]
+}
+
+
+{
+  "object": "page",
+  "entry": [
+    {
+      "time": 1739811716001,
+      "id": "144335542089831",
+      "messaging": [
+        {
+          "sender": {
+            "id": "6399073443526400"
+          },
+          "recipient": {
+            "id": "144335542089831"
+          },
+          "timestamp": 1739811715478,
+          "message": {
+            "mid": "m_LQr6ZBqb_aFnMFRuk__PaCcPHzTsTdbAhaPpG97qlO_bfcjf50xUvc0wsmg4a3yr636M8ZAg4X4CUMcw_Mu-Pw",
+            "attachments": [
+              {
+                "type": "video",
+                "payload": {
+                  "url": "https://video.xx.fbcdn.net/v/t42.3356-2/467187940_8827019697320071_3959013982902307873_n.mp4?_nc_cat=109&ccb=1-7&_nc_sid=4f86bc&_nc_ohc=0Dd1qexDgOgQ7kNvgEvRhcp&_nc_oc=Adib1RW2Ocp3nKFpmxfhX6JwDTIRUuWyV4aisPfDpbQmvMji8D5vimPh7CSMDAoVJzO3CPtExYs2dkZrGM-tL5po&_nc_zt=28&_nc_ht=video.xx&_nc_gid=ANSNhNLsnVvKoYugTAPV8PS&oh=03_Q7cD1gHenjAy7FgjxfCm1uZqHnhQS8dDquqnIV5reIBRV-2rEw&oe=67B54C39"
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  ]
+}
