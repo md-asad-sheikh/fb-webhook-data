2051d9928268e2810e59c38a594db11618a94848
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-02-17 22:46:42 +0600

    only text

---
+++ only-text.php
@@ -0,0 +1,24 @@
+{
+  "object": "page",
+  "entry": [
+    {
+      "time": 1739810636390,
+      "id": "144335542089831",
+      "messaging": [
+        {
+          "sender": {
+            "id": "6399073443526400"
+          },
+          "recipient": {
+            "id": "144335542089831"
+          },
+          "timestamp": 1739810635668,
+          "message": {
+            "mid": "m_wPnns5i0cIw2TMnC4Ut9cScPHzTsTdbAhaPpG97qlO9IEKOGPCj2DURX55nUZBnQVJQE_ea6VYI2CwdNTJM1RA",
+            "text": "hi"
+          }
+        }
+      ]
+    }
+  ]
+}
