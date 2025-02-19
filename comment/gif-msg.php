734277f4ef4d3ea338d5a25ff1536404042c5d45
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-02-19 22:57:15 +0600

    gif with text

---
+++ comment/gif-msg.php
@@ -0,0 +1,36 @@
+{
+  "entry": [
+    {
+      "id": "311430485396263",
+      "time": 1739983782,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "8841919032488258",
+              "name": "Md Asad Sheikh Sujat"
+            },
+            "post": {
+              "status_type": "added_photos",
+              "is_published": true,
+              "updated_time": "2025-02-19T16:49:37+0000",
+              "permalink_url": "https://www.facebook.com/permalink.php?story_fbid=pfbid0373y7LnPZmT5VdB6t3bKpFUTTBaENRRKPMS1wtE19Ujzm6dG8mN7bKhFSHwHWKEc3l&id=61562090259913",
+              "promotion_status": "inactive",
+              "id": "311430485396263_122113656170403008"
+            },
+            "message": "1234 🥹",
+            "post_id": "311430485396263_122113656170403008",
+            "video": "https://scontent.fzyl2-2.fna.fbcdn.net/v/t42.4659-2/468552618_8571380362969399_2529014040027982116_n.mp4?_nc_cat=109&ccb=1-7&_nc_sid=a3d3d5&_nc_eui2=AeG3I6lxvpSR49MNEOX8YSBr2Tsusejt4-PZOy6x6O3j40t-spMh04DnBopZzrZhrSVhheAsuImGfjQA0WBVzTwg&_nc_ohc=RNzPKaOrFVkQ7kNvgHYBqgW&_nc_oc=AdjksU57IE_5smlarZXC7TyJEfL7BTBMTiLtG7KvXnWpZH-DDDgE_B5QnHkB_3tT2s0&_nc_zt=28&_nc_ht=scontent.fzyl2-2.fna&_nc_gid=AN9NnzkzVxx9_QSHc9nlxcq&oh=00_AYAQ9WJPI02h3uck79xHwHTmQ87mha3G9Ila5xPsTpglUw&oe=67B63185",
+            "comment_id": "122113656170403008_554359620983952",
+            "created_time": 1739983777,
+            "item": "comment",
+            "parent_id": "311430485396263_122113656170403008",
+            "verb": "add"
+          },
+          "field": "feed"
+        }
+      ]
+    }
+  ],
+  "object": "page"
+}
