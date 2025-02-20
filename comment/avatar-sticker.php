58060dfa311ed8aa4293e9eef13bfd43607520ff
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-02-20 22:11:47 +0600

    avatar sticker

---
+++ comment/avatar-sticker.php
@@ -0,0 +1,34 @@
+{
+  "entry": [
+    {
+      "id": "311430485396263",
+      "time": 1740067763,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "8841919032488258",
+              "name": "Md Asad Sheikh Sujat"
+            },
+            "post": {
+              "status_type": "added_photos",
+              "is_published": true,
+              "updated_time": "2025-02-20T16:09:19+0000",
+              "permalink_url": "https://www.facebook.com/permalink.php?story_fbid=pfbid0ZLerV9KfH3k9d1CXnHRDnG452kj3yXBHs93V61K4gf9NqjntWDRycnJKSFtLuJ7ul&id=61562090259913",
+              "promotion_status": "inactive",
+              "id": "311430485396263_122113651010403008"
+            },
+            "post_id": "311430485396263_122113651010403008",
+            "comment_id": "122113651010403008_653397293888912",
+            "created_time": 1740067759,
+            "item": "comment",
+            "parent_id": "311430485396263_122113651010403008",
+            "verb": "add"
+          },
+          "field": "feed"
+        }
+      ]
+    }
+  ],
+  "object": "page"
+}
