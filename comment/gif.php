744a75f658f1ad3ff12519bc5ac5fe32b0e27aec
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-02-23 23:35:00 +0600

    gif

---
+++ comment/gif.php
@@ -0,0 +1,35 @@
+{
+  "entry": [
+    {
+      "id": "222410637619276",
+      "time": 1740332016,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "6845011145625998",
+              "name": "Md Asad Sheikh Sujat"
+            },
+            "post": {
+              "status_type": "added_video",
+              "is_published": true,
+              "updated_time": "2025-02-23T17:33:33+0000",
+              "permalink_url": "https://www.facebook.com/122141481680188139/videos/1034859511746038",
+              "promotion_status": "inactive",
+              "id": "222410637619276_122190013970188139"
+            },
+            "post_id": "222410637619276_122190013970188139",
+            "video": "https://scontent.fzyl2-2.fna.fbcdn.net/v/t42.4659-2/475792057_28327965763517397_901652523921426114_n.mp4?_nc_cat=105&ccb=1-7&_nc_sid=a3d3d5&_nc_eui2=AeF42LGRml8V1PnzzYDwMpjBxaouLxPY4tzFqi4vE9ji3HFSfxXdboHgip3m3BDM9GXkBPnMCdtZFJ2oyvHUkfmK&_nc_ohc=dnEVvB0sEPAQ7kNvgFF97IC&_nc_oc=Adj94_hx89x4dxonPuJRGmrVjy_AkG2A0gMCViyzk8C3LZGSbFkuy7_8oO8xjl_UsGA&_nc_zt=28&_nc_ht=scontent.fzyl2-2.fna&_nc_gid=Aa6GinpslZdcgO6qy_gRC9P&oh=00_AYD0D6yl23XrMKTpentwaa2OOHHDSd1P5oGdpeJUVBgNDw&oe=67BB7852",
+            "comment_id": "122190013970188139_1331867441278419",
+            "created_time": 1740332013,
+            "item": "comment",
+            "parent_id": "222410637619276_122190013970188139",
+            "verb": "add"
+          },
+          "field": "feed"
+        }
+      ]
+    }
+  ],
+  "object": "page"
+}
