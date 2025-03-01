0106f2d5c9bf22eb7884c8dbfb5aee808b4c3180
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-03-01 21:30:32 +0600

    add sad reaction in msg

---
+++ message/reaction.php
@@ -0,0 +1,26 @@
+{
+  "object": "page",
+  "entry": [
+    {
+      "time": 1740842731786,
+      "id": "144335542089831",
+      "messaging": [
+        {
+          "sender": {
+            "id": "6399073443526400"
+          },
+          "recipient": {
+            "id": "144335542089831"
+          },
+          "timestamp": 1740842731116,
+          "reaction": {
+            "mid": "m_NXhI6h8SljL1ipD1hic-BicPHzTsTdbAhaPpG97qlO9Xt80UAz6VMHik5NsQ3fDTna7lbIZkBwW_9ud8Xf72sQ",
+            "action": "react",
+            "emoji": "😢",
+            "reaction": "sad"
+          }
+        }
+      ]
+    }
+  ]
+}
