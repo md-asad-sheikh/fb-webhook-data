4c878f343038c6872321cb81fe99b5e8ebbb7eb3
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-03-01 21:35:16 +0600

    msg reply to

---
+++ message/reply-to.php
@@ -0,0 +1,27 @@
+{
+  "object": "page",
+  "entry": [
+    {
+      "time": 1740843106989,
+      "id": "144335542089831",
+      "messaging": [
+        {
+          "sender": {
+            "id": "6399073443526400"
+          },
+          "recipient": {
+            "id": "144335542089831"
+          },
+          "timestamp": 1740843106347,
+          "message": {
+            "mid": "m_ld7ExW9UkX6iKQIIKC1APicPHzTsTdbAhaPpG97qlO-rtKzQ7FuTQupeUBVbc1wZHxsRDkPxzzXV8cxXKUmRWg",
+            "text": "very nice",
+            "reply_to": {
+              "mid": "m_NXhI6h8SljL1ipD1hic-BicPHzTsTdbAhaPpG97qlO9Xt80UAz6VMHik5NsQ3fDTna7lbIZkBwW_9ud8Xf72sQ"
+            }
+          }
+        }
+      ]
+    }
+  ]
+}
