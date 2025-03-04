456598a7e356d44faf3bf0c53dbb636fa70da178
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-03-04 16:09:52 +0600

    profile comment

---
+++ comment/cmt/profile-has-desc-1-cmt.php
@@ -0,0 +1,58 @@
+{
+  "entry": [
+    {
+      "id": "144335542089831",
+      "time": 1741077784,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "8775752665887443",
+              "name": "Ihelpbd Asad"
+            },
+            "post": {
+              "status_type": "added_photos",
+              "is_published": true,
+              "updated_time": "2025-03-04T08:42:59+0000",
+              "permalink_url": "https://www.facebook.com/photo/?fbid=122185378376057808&set=a.122100772628057808",
+              "promotion_status": "ineligible",
+              "id": "144335542089831_122185378376057808"
+            },
+            "message": "very nice",
+            "post_id": "144335542089831_122185378376057808",
+            "comment_id": "122185378376057808_626116470029102",
+            "created_time": 1741077779,
+            "item": "comment",
+            "parent_id": "7201897446541241_122185378376057808",
+            "verb": "add"
+          },
+          "field": "feed"
+        }
+      ]
+    },
+    {
+      "id": "144335542089831",
+      "time": 1741077784,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "8775752665887443",
+              "name": "Ihelpbd Asad"
+            },
+            "link": "https://scontent.fdac5-2.fna.fbcdn.net/v/t39.30808-6/481177672_122185378382057808_35687452721915582_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=d0a8c7&_nc_ohc=0qy6yn9-81sQ7kNvgFLEx14&_nc_oc=AdgOVRhkcP1LSZD6eELXF54sTFIkVkug4qOyMNQN0MIkTk2U50EbrQUuR5gwjq14t2g&_nc_zt=23&_nc_ht=scontent.fdac5-2.fna&_nc_gid=AwPbceHpt46QN9jAvxQnGCM&oh=00_AYBUu9LvgDByPqoKudXobRfE-Lnw_bFwsimXTwPsRRFsHw&oe=67CCA5B0",
+            "message": "aesthetic sea side",
+            "post_id": "144335542089831_122185378376057808",
+            "created_time": 1741077755,
+            "item": "photo",
+            "photo_id": "122185378376057808",
+            "published": 1,
+            "verb": "edited"
+          },
+          "field": "feed"
+        }
+      ]
+    }
+  ],
+  "object": "page"
+}
---
+++ comment/cmt/profile-has-desc-2-cmt.php
@@ -0,0 +1,35 @@
+{
+  "entry": [
+    {
+      "id": "144335542089831",
+      "time": 1741078584,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "8775752665887443",
+              "name": "Ihelpbd Asad"
+            },
+            "post": {
+              "status_type": "added_photos",
+              "is_published": true,
+              "updated_time": "2025-03-04T08:56:21+0000",
+              "permalink_url": "https://www.facebook.com/photo/?fbid=122185378376057808&set=a.122100772628057808",
+              "promotion_status": "ineligible",
+              "id": "144335542089831_122185378376057808"
+            },
+            "message": "pp has description - 1",
+            "post_id": "144335542089831_122185378376057808",
+            "comment_id": "122185378376057808_1175350744031828",
+            "created_time": 1741078581,
+            "item": "comment",
+            "parent_id": "7201897446541241_122185378376057808",
+            "verb": "add"
+          },
+          "field": "feed"
+        }
+      ]
+    }
+  ],
+  "object": "page"
+}
