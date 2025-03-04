646a43aa28b475cb5b8ebb9cb2373bb787b69255
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-03-04 14:37:47 +0600

    text reply

---
+++ comment/cmt/text-reply.php
@@ -0,0 +1,35 @@
+{
+  "entry": [
+    {
+      "id": "144335542089831",
+      "time": 1741070749,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "6399073443526400",
+              "name": "Md Asad Sheikh Sujat"
+            },
+            "post": {
+              "status_type": "added_photos",
+              "is_published": true,
+              "updated_time": "2025-03-04T06:45:42+0000",
+              "permalink_url": "https://www.facebook.com/permalink.php?story_fbid=pfbid0ZTKcN2xRWAQzJoyHYoXppuyuT5KzwMoa5G6zhHWX1xUbgC4vJV7VWvcSY847zNzDl&id=61551734259596",
+              "promotion_status": "inactive",
+              "id": "144335542089831_122183509706057808"
+            },
+            "message": "reply 1 for text",
+            "post_id": "144335542089831_122183509706057808",
+            "comment_id": "122183509706057808_1200705341415663",
+            "created_time": 1741070742,
+            "item": "comment",
+            "parent_id": "122183509706057808_639917085641344",
+            "verb": "add"
+          },
+          "field": "feed"
+        }
+      ]
+    }
+  ],
+  "object": "page"
+}
