210f685712685202b7c542f20d4387098e5746ee
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-03-04 12:25:33 +0600

    profile comment

---
+++ comment/cmt/profile.php
@@ -0,0 +1,35 @@
+{
+  "entry": [
+    {
+      "id": "144335542089831",
+      "time": 1741069414,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "6399073443526400",
+              "name": "Md Asad Sheikh Sujat"
+            },
+            "post": {
+              "status_type": "added_photos",
+              "is_published": true,
+              "updated_time": "2025-03-04T06:23:30+0000",
+              "permalink_url": "https://www.facebook.com/photo/?fbid=122130354704057808&set=a.122100772628057808",
+              "promotion_status": "ineligible",
+              "id": "144335542089831_122130354704057808"
+            },
+            "message": "pp cmt 2",
+            "post_id": "144335542089831_122130354704057808",
+            "comment_id": "122130354704057808_1252535426502048",
+            "created_time": 1741069410,
+            "item": "comment",
+            "parent_id": "7201897446541241_122130354704057808",
+            "verb": "add"
+          },
+          "field": "feed"
+        }
+      ]
+    }
+  ],
+  "object": "page"
+}
