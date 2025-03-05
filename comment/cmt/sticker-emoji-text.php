fa6736a8f38f3ac912cd2b16706a8a7f87bbb72d
md-asad-sheikh <mdasad.sheikh.diu@gmail.com>
2025-03-05 22:59:17 +0600

    text & sticker comment

---
+++ comment/cmt/sticker-emoji-text.php
@@ -0,0 +1,35 @@
+{
+  "entry": [
+    {
+      "id": "144335542089831",
+      "time": 1741193865,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "6399073443526400",
+              "name": "Md Asad Sheikh Sujat"
+            },
+            "post": {
+              "status_type": "added_photos",
+              "is_published": true,
+              "updated_time": "2025-03-05T16:57:42+0000",
+              "permalink_url": "https://www.facebook.com/permalink.php?story_fbid=pfbid02dJmKYhtFMYAmR6n1CSfy7RTqnihnmAGVwGFtNXV6fk88rGZyrAxEk9yUtDzhtjWtl&id=61551734259596",
+              "promotion_status": "inactive",
+              "id": "144335542089831_122183509706057808"
+            },
+            "message": "😙 reply to sleepy sticker in bird",
+            "post_id": "144335542089831_122183509706057808",
+            "comment_id": "122183509706057808_635624462522325",
+            "created_time": 1741193862,
+            "item": "comment",
+            "parent_id": "122183509706057808_1303518447570771",
+            "verb": "add"
+          },
+          "field": "feed"
+        }
+      ]
+    }
+  ],
+  "object": "page"
+}
---
+++ comment/cmt/sticker-emoji.php
@@ -0,0 +1,35 @@
+{
+  "entry": [
+    {
+      "id": "144335542089831",
+      "time": 1741193705,
+      "changes": [
+        {
+          "value": {
+            "from": {
+              "id": "6399073443526400",
+              "name": "Md Asad Sheikh Sujat"
+            },
+            "post": {
+              "status_type": "added_photos",
+              "is_published": true,
+              "updated_time": "2025-03-05T16:55:01+0000",
+              "permalink_url": "https://www.facebook.com/permalink.php?story_fbid=pfbid02dJmKYhtFMYAmR6n1CSfy7RTqnihnmAGVwGFtNXV6fk88rGZyrAxEk9yUtDzhtjWtl&id=61551734259596",
+              "promotion_status": "inactive",
+              "id": "144335542089831_122183509706057808"
+            },
+            "message": "😍",
+            "post_id": "144335542089831_122183509706057808",
+            "comment_id": "122183509706057808_1303518447570771",
+            "created_time": 1741193701,
+            "item": "comment",
+            "parent_id": "144335542089831_122183509706057808",
+            "verb": "add"
+          },
+          "field": "feed"
+        }
+      ]
+    }
+  ],
+  "object": "page"
+}
